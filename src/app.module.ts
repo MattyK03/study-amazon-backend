807cd1c1951ba8ebdf4fafe6b300639ef3fb1e0f
Matty <mask0343@gmail.com>
2023-03-30 17:59:28 +0300

    createdproduct, order, review, category

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,14 @@ import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { PrismaService } from './prisma.service'
 import { UserModule } from './user/user.module'
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
+import { CategoryModule } from './category/category.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
 	controllers: [AppController],
 	providers: [AppService, PrismaService],
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule]
+	imports: [ConfigModule.forRoot(), AuthModule, UserModule, ProductModule, ReviewModule, CategoryModule, OrderModule]
 })
 export class AppModule {}
