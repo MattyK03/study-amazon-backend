1564ba718cd0a0ad69345f599157bf851e1d6f82
Matty <mask0343@gmail.com>
2023-04-16 22:03:37 +0300

    add avg by product reques to review

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -34,4 +34,9 @@ export class ReviewController {
 	) {
 		return this.reviewService.create(id, +productId, dto)
 	}
+
+	@Get('average-by-product/:productId')
+	async getAverageByProduct(@Param('productId') productId: string) {
+		return this.reviewService.getAverageValueByProductId(+productId)
+	}
 }
